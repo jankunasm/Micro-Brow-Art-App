7b26a7cd194a2935752ea7b11c3fb1b095d5a473
jankunasm <jankunasmatas11@gmail.com>
2021-12-07 14:18:33 -0600

    First commit for final project.

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Giving access to the project in any Operating System
+# Allow outside files/folders to be added to the project
+# base directory
+
+class Config():
+    """
+        Set Config variables for the flask app.
+        Using Environment variables where available
+        create config variables if not done already.
+    """
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never get this!'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off messages for updates in sqlalchemy
